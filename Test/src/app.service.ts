60e0170d96714d8cb26aa4cae1424d269238cf6b
hamidkrml <karimlihamid4@gmail.com>
2025-07-25 02:06:47 +0400

    Add new endpoint and implement lifecycle hooks in AppService

--- Test/src/app.service.ts
+++ Test/src/app.service.ts
@@ -1,8 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  OnApplicationBootstrap,
+  OnApplicationShutdown,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class AppService
+  implements
+    OnModuleInit,
+    OnApplicationBootstrap,
+    OnModuleDestroy,
+    OnApplicationShutdown
+{
+  onModuleDestroy() {
+    throw new Error('Method not implemented.');
+  }
+  onApplicationShutdown(signal?: string) {
+    throw new Error('Method not implemented.');
+  }
+
+  onApplicationBootstrap() {
+    console.log('onApllicationBootst');
+  }
+  onModuleInit() {
+    console.log('Apservice baslatildi ');
+  }
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!!!';
+  }
+
+  Gethii(): string {
+    return 'GetHii!';
   }
 }
